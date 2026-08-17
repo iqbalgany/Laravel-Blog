b5150c3622193aa8f98e9e543d616aca2fde2bef
iqbalgany <rismagany1203@gmail.com>
2026-08-17 23:12:53 +0700

    feat: add dashboard controller and UI components

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,4 @@
 <x-layout :title="$title">
-    {{-- <article class="py-6 max-w-3xl-md border-b border-gray-300">
-        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-        <div class="text-gray-500">
-            <a href="/authors/{{ $post->author->username }}" class="hover:underline">{{ $post->author->name }}</a> | 1
-            Januari 2026
-        </div>
-        <p class="my-4 font-light">{{ $post['body'], 100 }}</p>
-        <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to all posts</a>
-    </article> --}}
-
-
     <main class="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
         <div class="flex justify-between px-4 mx-auto max-w-screen-xl ">
             <article
